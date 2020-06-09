176b2cb7f78e2fb7fc2c0f9526e8bd3aab006520
Alicia <btfmw423@yahoo.co.jp>
2020-06-09 19:14:58 +0000

    Finish add-basic-info

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,14 @@
 class UsersController < ApplicationController
-  before_action :set_user,only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   #before-action(Ruby on Rails 簡単なSNSアプリを作ろう８章8.9に説明あり参照)の設定する必要理由
   #→⓵本来ユーザー情報更新ページに行くにはログインする必要がある
   #➁しかしトップページのURLの末尾に ex  users/1/edit と入力するとログインなしで飛べてしまう
   #➂それではセキュリティ上問題があるので各アクションに飛ぶまえにbefore-actionを設定し
   #ログインなしでユーザー情報更新ページへ飛ぶことができるのを回避しようとする為に設定
-  before_action :logged_in_user, only: [:index, :show, :edit, :update,:destroy]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
   #correct_userメソッドを定義し、アクセスしたユーザーが現在ログインしているユーザーであるか確認するよう判定
   before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: :destroy
+  before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
   #indexの中身詳細:勤怠8章8.4.2参照
   def index
     @users = User.paginate(page: params[:page])
@@ -49,12 +49,33 @@ class UsersController < ApplicationController
     flash[:success] = "#{@user.name}のデータを削除しました。"
     redirect_to users_url
   end
+  
+  def edit_basic_info
+  end
+  
+  def update_basic_info
+    if @user.update_attributes(basic_info_params)
+      flash[:success] = "#{@user.name}の基本情報を更新しました。"
+    else
+      flash[:danger] = "#{@user.name}の更新は失敗しました。<br>" + @user.errors.full_messages.join("<br>")
+    end
+    redirect_to users_url
+  end
 
   private
-
+    
+    # user_paramsとbasic_info_paramsの補足　下記
+    # ↓Strong Parametersを用いることで、必須となるパラメータと許可されたパラメータを指定することができます。
+    # つまり下記の様に指定することにより、下記のカラムのみデータが取得できる
+    # (他の重要なカラムは取得できないようにする)
     def user_params
       params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
     end
+    
+    def basic_info_params
+      params.require(:user).permit(:department, :basic_time, :work_time)
+    end
+    
 
     # beforeフィルター(logged_in_userメソッドはprivateキーワード下に定義しました)
 
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,13 @@ Rails.application.routes.draw do
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
-  resources :users
+  # memberはメンバールーティング（photos/:id/previewのようにidを伴うパス）を追加するときに使う。
+  # memberブロックをリソースブロック(resources :~~ do)の中に1つ追加する。追加したブロック内にメンバルーティングを記述する。
+  # 参照:https://qiita.com/senou/items/f1491e53450cb347606b
+  resources :users do
+    member do
+      get 'edit_basic_info'
+      patch 'update_basic_info'
+    end
+  end
 end
