dd4f387c80a88451a1d8b12d9b2b6be47d7dc807
Alicia <btfmw423@yahoo.co.jp>
2020-06-12 14:25:51 +0000

    勤怠10章10.6 終了時点

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,4 +1,7 @@
 class AttendancesController < ApplicationController
+  # ↓定数→変数とは違い、値を変えることはできません。大文字表記
+  UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
+  
   def update
     @user = User.find(params[:user_id])
     @attendance = Attendance.find(params[:id])
@@ -9,7 +12,13 @@ class AttendancesController < ApplicationController
       if @attendance.update_attributes(started_at: Time.current.change(sec: 0))
         flash[:info] = "おはようございます！"
       else
-        flash[:danger] = "勤怠登録に失敗しました。やり直してください。"
+        flash[:danger] = UPDATE_ERROR_MSG
+      end
+    elsif @attendance.finished_at.nil?
+      if @attendance.update_attributes(finished_at: Time.current.change(sec: 0))
+        flash[:info] = "お疲れ様でした。"
+      else
+        flash[:danger] = UPDATE_ERROR_MSG
       end
     end
     redirect_to @user
--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -1,2 +1,17 @@
 module AttendancesHelper
+  # テキスト勤怠10.6参照：渡される引数は、繰り返し処理されているAttendanceモデルオブジェクトを想定しています。
+  # このメソッドを使って勤怠登録ボタンのテキストには何が必要か？または必要ないか？」をこのメソッドで判定することです。
+  # このメソッドでは戻り値が3パターン ⓵「勤怠データが当日、かつ出勤時間が存在しない場合」に'出勤'
+  # ➁勤怠データが当日、かつ出勤時間が登録済、かつ退勤時間が存在しない場合」に'退勤'
+  # ➂「勤怠データが当日ではない、または当日だが出勤時間も退勤時間も登録済の場合」にfalse  →何も起こらない
+  def attendance_state(attendance)
+    # 受け取ったAttendanceオブジェクトが当日と一致するか評価します。
+    if Date.current == attendance.worked_on
+      return '出勤' if attendance.started_at.nil?
+      return '退勤' if attendance.started_at.present? && attendance.finished_at.nil?
+    end
+    # どれにも当てはまらなかった場合はfalseを返します。
+    false
+  end
+
 end
